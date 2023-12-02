2d5ac2f481aa8a4fea02dbb9183c9e7e7f49e1bf
Suneel Kumar Yaram <syaram@vaudoise.ch>
2023-12-02 14:18:04 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider
+terraform {
+  cloud {
+    organization = "SuneelOrg"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "switzerland north"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
